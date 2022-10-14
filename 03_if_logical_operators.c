48298fc7d008f2490cd5dc71dd3f710c62e6ced4
mohit1233k <58824274+mohit1233k@users.noreply.github.com>
2022-10-14 22:29:58 +0530

    Update 03_if_logical_operators.c

--- 03_if_logical_operators.c
+++ 03_if_logical_operators.c
@@ -9,13 +9,13 @@ int main(){
     scanf("%d", &age); 
 
     // if (age <= 70 && age>=18)
-    if ((age <= 70 && age>=18) || (vipPass==1))
+    if ((age <= 90 && age>=18) || (vipPass==1))
     {
         printf("You are above 18 and below 70, you can drive\n");
     }
     else
     {
-        printf("You cannot drive\n");
+        printf("You cannot drive a car\n");
     }
    
     return 0;
